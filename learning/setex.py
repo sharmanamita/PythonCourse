92304af0c5aa4c960fbf1091e304a0d0472cffef
sharmanamita <namitasharma6@outlook.com>
2026-02-24 18:02:10 +0530

    Interview questions

--- learning/setex.py
+++ learning/setex.py
@@ -1,15 +1,15 @@
-mylist = [ 15, 25, 63, 2, 8, 2]
+mylist = [15, 25, 63, 2, 8, 2]
 
 for x in mylist:
-	print(x)
-	
-print("print set")	
-myset = { 1, 5, 2, 75, 15, 2, 3, 3, 'namita'}
+    print(x)
 
+print("print set")
+myset = {1, 5, 2, 75, 15, 2, 3, 3, "namita"}
 for x in myset:
-	print(x)
-	
+    print(x)
+
 print("Print tuple")
 mytuple = ("namita", "Pune", 19, 1997)
+tuple(mytuple, 12, 23, 24)
 mytuple.add(50)
 print(mytuple)
